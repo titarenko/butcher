881148866be69f36a9d89c9ba528229de86ee0ec
Constantin Titarenko <constantin.titarenko@gmail.com>
2014-10-13 18:06:36 +0300

    Take into account reverse proxying

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var body = require('body/json')
 
 http.createServer(function (req, res) {
 	body(req, res, function (err, body) {
-		console.log(new Date(), req.connection.remoteAddress, req.url, err, body);
+		console.log(new Date(), req.headers['x-forwarded-for'] || req.connection.remoteAddress, req.url, err, body);
 		res.end();
 	});
 }).listen(process.env.PORT || 6000);
