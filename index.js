9602c85f8ca10f0ac656372df0d525c95f3c9c5b
Constantin Titarenko <constantin.titarenko@gmail.com>
2017-03-22 19:44:53 +0200

    cleanup root folder

--- index.js
+++
@@ -1,9 +0,0 @@
-var http = require('http');
-var body = require('body/json')
-
-http.createServer(function (req, res) {
-	body(req, res, function (err, body) {
-		console.log(new Date(), req.headers['x-forwarded-for'] || req.connection.remoteAddress, req.url, req.headers, err, body);
-		res.end();
-	});
-}).listen(process.env.PORT || 6000);
