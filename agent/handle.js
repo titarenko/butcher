4c9c34ac1c5cfb4661b39f3a3a4a99ea2b4cd824
Constantin Titarenko <constantin.titarenko@gmail.com>
2017-03-22 21:48:58 +0200

    move handling out of main module

---
+++ agent/handle.js
@@ -0,0 +1,33 @@
+const execute = require('./execute')
+const { repository, branch } = require('./config')
+
+module.exports = handle
+
+function handle (data, send) {
+	const commandText = data.toString()
+
+	Promise
+		.try(() => {
+			const command = JSON.parse(commandText)
+			if (repository && command.repository.name != repository
+				|| branch && command.branch != branch) {
+				throw new Error(`illegal command ${commandText}`)
+			}
+			return command
+		})
+		.then(command => execute({ command, onFeedback: data => handleFeedback(command, data) }))
+		.catch(handleFailure)
+
+	function handleFeedback (command, data) {
+		client.send({ type: 'FEEDBACK', command, data })
+	}
+
+	function handleFailure (error) {
+		log.error(`failed to execute ${commandText} due to ${error.stack}`)
+		try {
+			client.send({ type: 'FAILURE', command: JSON.parse(commandText) })
+		} catch (sendError) {
+			log.error(`failed to send failure due to ${sendError.stack}`)
+		}
+	}
+}
--- agent/index.js
+++ agent/index.js
@@ -1,42 +1,17 @@
-const net = require('net')
 const Promise = require('bluebird')
+const handle = require('./handle')
+const log = require('totlog')(__filename)
+const { host, port, role, password, repository, branch } = require('./config')
 
 Promise.longStackTraces()
 
-const execute = require('./execute')
-const log = require('totlog')(__filename)
-
-const { host, port, role, password, repository, branch } = require('./config')
-
 const client = new net.Socket()
 
-client.on('data', data => {
-	const commandText = data.toString()
-
-	Promise
-		.try(() => {
-			const command = JSON.parse(commandText)
-			if (repository && command.repository.name != repository
-				|| branch && command.branch != branch) {
-				throw new Error(`illegal command ${commandText}`)
-			}
-			return command
-		})
-		.then(command => execute({ command, onFeedback: data => handleFeedback(command, data) }))
-		.catch(handleFailure)
-
-	function handleFeedback (command, data) {
-		send({ type: 'FEEDBACK', command, data })
-	}
-
-	function handleFailure (error) {
-		log.error(`failed to execute ${commandText} due to ${error.stack}`)
-		try {
-			send({ type: 'FAILURE', command: JSON.parse(commandText) })
-		} catch (sendError) {
-			log.error(`failed to send failure due to ${sendError.stack}`)
-		}
-	}
+client.on('data', data => handle(data, send))
+client.on('close', shutdown)
+client.on('error', error => {
+	log.error(`connection failed due to ${error.stack}`)
+	shutdown()
 })
 
 client.connect(port, host, () => {
@@ -50,11 +25,6 @@ function send (message) {
 }
 
 process.on('SIGINT', shutdown)
-client.on('close', shutdown)
-client.on('error', error => {
-	log.error(`connection failed due to ${error.stack}`)
-	shutdown()
-})
 
 function shutdown () {
 	if (!client.destroyed) {
