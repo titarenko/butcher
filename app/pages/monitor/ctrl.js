b4a27d249a3919fa63d3eebac457828aa4093682
Constantin Titarenko <constantin.titarenko@gmail.com>
2017-03-27 14:13:44 +0300

    monitor page

--- app/pages/monitor/ctrl.js
+++ app/pages/monitor/ctrl.js
@@ -1,9 +1,22 @@
 const pg = require('../../pg')
-const agents = require('../../engine/agents')
 
 module.exports = {
-	agents: () => agents.list(),
+	agents: () => pg('agents').orderBy('connected_at', 'desc'),
 	repositories: () => pg('repositories').orderBy('updated_at', 'desc'),
-	branches: () => pg('branches').orderBy('updated_at', 'desc'),
-	executions: () => pg('executions').orderBy('updated_at', 'desc'),
+	branches: () => pg('branches as b')
+		.select('b.*', 'r.name as repository')
+		.join('repositories as r', 'r.id', 'b.repository_id')
+		.orderBy('b.updated_at', 'desc'),
+	executions: () => pg('executions as e')
+		.select(
+			'e.*',
+			'e.created_at as started_at',
+			'r.name as repository',
+			'b.name as branch',
+			pg.raw("command->>'commit' as commit")
+		)
+		.join('branches as b', 'b.id', 'e.branch_id')
+		.join('repositories as r', 'r.id', 'b.repository_id')
+		.orderBy('e.updated_at', 'desc')
+		.limit(20),
 }
--- app/pages/monitor/index.jsx
+++ app/pages/monitor/index.jsx
@@ -15,57 +15,54 @@ module.exports.reducer = combineReducers({
 
 function Monitor ({ repositories, branches, executions, agents, dispatch }) {
 	return <div className="monitor">
-		<section>
-			<h2>repositories</h2>
-			<List
-				{...repositories}
-				resource="/api/monitor.repositories"
-				Table={Repositories}
-				dispatch={createDispatch('repositories', dispatch)} />
-		</section>
-		<section>
-			<h2>branches</h2>
-			<List
-				{...branches}
-				resource="/api/monitor.branches"
-				Table={Branches}
-				dispatch={createDispatch('branches', dispatch)} />
-		</section>
-		<section>
-			<h2>executions</h2>
+		<article>
 			<List
 				{...executions}
 				resource="/api/monitor.executions"
 				Table={Executions}
 				dispatch={createDispatch('executions', dispatch)} />
-		</section>
-		<section>
-			<h2>agents</h2>
-			<List
-				{...agents}
-				resource="/api/monitor.agents"
-				Table={Agents}
-				dispatch={createDispatch('agents', dispatch)} />
-		</section>
+		</article>
+		<aside>
+			<section>
+				<h2>agents</h2>
+				<List
+					{...agents}
+					resource="/api/monitor.agents"
+					Table={Agents}
+					dispatch={createDispatch('agents', dispatch)} />
+			</section>
+			<section>
+				<h2>repositories</h2>
+				<List
+					{...repositories}
+					resource="/api/monitor.repositories"
+					Table={Repositories}
+					dispatch={createDispatch('repositories', dispatch)} />
+			</section>
+			<section>
+				<h2>branches</h2>
+				<List
+					{...branches}
+					resource="/api/monitor.branches"
+					Table={Branches}
+					dispatch={createDispatch('branches', dispatch)} />
+			</section>
+		</aside>
 	</div>
 
 	function Executions ({ items }) {
-		return <table>
-			<thead>
-				<tr>
-					<th>start</th>
-					<th>update</th>
-					<th>finish</th>
-					<th>feedback</th>
-				</tr>
-			</thead>
-			<tbody>{items.map(it => <tr>
-				<td>{moment(it.created_at).format('MMM DD, HH:mm')}</td>
-				<td>{moment(it.updated_at).format('mm:ss')}</td>
-				<td>{moment(it.finished_at).format('mm:ss')}</td>
-				<td>{it.feedback}</td>
-			</tr>)}</tbody>
-		</table>
+		return <div>{items.map(it => <div className="execution">
+			<div className={`props ${it.is_failed ? 'failed' : 'succeeded'}`}>
+				<div>
+					<i className="fa fa-clock-o" aria-hidden="true"></i><span> </span>
+					{moment(it.finished_at || it.updated_at || it.started_at).format('MMM DD, HH:mm:ss')}
+				</div>
+				<div><i className="fa fa-github" aria-hidden="true"></i> {it.repository}</div>
+				<div><i className="fa fa-code-fork" aria-hidden="true"></i> {it.branch}</div>
+				<div><i className="fa fa-hashtag" aria-hidden="true"></i> {it.commit}</div>
+			</div>
+			<pre>{it.feedback}</pre>
+		</div>)}</div>
 	}
 
 	function Agents ({ items }) {
@@ -73,27 +70,51 @@ function Monitor ({ repositories, branches, executions, agents, dispatch }) {
 			<thead>
 				<tr>
 					<th>name</th>
-					<th>role</th>
+					<th>connected</th>
+					<th>stage</th>
 					<th title="repository">rep.</th>
 					<th title="branch">br.</th>
-					<th>connected</th>
 				</tr>
 			</thead>
 			<tbody>{items.map(it => <tr>
 				<td title={it.ip}>{it.name || it.ip}</td>
-				<td>{it.role}</td>
+				<td title={moment(it.connected_at).fromNow()}>{moment(it.connected_at).format('MMM DD, HH:mm')}</td>
+				<td>{it.stage || <em>any</em>}</td>
 				<td>{it.repository || <em>any</em>}</td>
 				<td>{it.branch || <em>any</em>}</td>
-				<td title={moment(it.connected_at).fromNow()}>{moment(it.connected_at).format('MMM DD, HH:mm')}</td>
 			</tr>)}</tbody>
 		</table>
 	}
 
 	function Repositories ({ items }) {
-		return <ul>{items.map(it => <li>{it.name}</li>)}</ul>
+		return <table>
+			<thead>
+				<tr>
+					<th>name</th>
+					<th>last upd.</th>
+				</tr>
+			</thead>
+			<tbody>{items.map(it => <tr>
+				<td>{it.name}</td>
+				<td>{moment(it.updated_at || it.created_at).format('MMM DD, HH:mm:ss')}</td>
+			</tr>)}</tbody>
+		</table>
 	}
 
 	function Branches ({ items }) {
-		return <ul>{items.map(it => <li>{it.name}</li>)}</ul>
+		return <table>
+			<thead>
+				<tr>
+					<th>rep.</th>
+					<th>name</th>
+					<th>last upd.</th>
+				</tr>
+			</thead>
+			<tbody>{items.map(it => <tr>
+				<td>{it.repository}</td>
+				<td>{it.name}</td>
+				<td>{moment(it.updated_at || it.created_at).format('MMM DD, HH:mm:ss')}</td>
+			</tr>)}</tbody>
+		</table>
 	}
 }
